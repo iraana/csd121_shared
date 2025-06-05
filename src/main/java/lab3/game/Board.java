cd37cd788526fe25c06a4625102e5535bb1c35c0
karsten705 <07028074@saultcollege.ca>
2025-06-05 14:34:10 -0400

    Board.java and Console.java methods update by Karsten

--- src/main/java/lab3/game/Board.java
+++ src/main/java/lab3/game/Board.java
@@ -18,9 +18,72 @@ public class Board {
     // TODO:
     //  - what methods do we need?
     //  - check row, column, diagonals for a win?
+    public char checkWinner() {
+        //row
+        for(int i=0; i<3; i++) {
+            // checking for row win
+            if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return board[i][0];
+            }
+            // checking for column win
+            if (board[0][i] != ' ' && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+                return board[0][i];
+            }
+            //diagonal: from top-left to bottom-right
+            if (board[0][0]!=' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+                return board[0][0];
+            }
+            //diagonal: from top-right to bottom-left
+            if (board[0][2]!=' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+                return board[0][2];
+            }
+        }
+        return ' ';
+    }
     //  - method to place the player symbol on the board(only of that position is not taken  so we have to add a check if the cell is free)
+    public boolean placeMove(int row, int col, char player) {
+        if (row >= 0 && row <3 && col >=0 && col < 3 && board[row][col] == ' ') {
+            board[row][col] = player;
+            return true;
+        }
+        return false;
+    }
     //  - check if board is full, and we have no winner and its a draw
+    public boolean isFull() {
+        for (char[] row: board) {
+            for (char cell : row) {
+                if (cell == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    // Draw conditions:
+    // 1. The Board should be full - use isFull()
+    // 2. There should not be any winner - checkWinner() == ''
+    public boolean checkDraw() {
+        return isFull() && checkWinner() == ' ';
+        // 1. True && True -> True
+        // 2. False && True -> False
+    }
     //  - also here can create a method to print a board into console after each move
+    public void printBoard() {
+        System.out.println("Current State of Board");
+        System.out.println("---------------------");
+        for(int i=0; i<3; i++) {
+            System.out.print("| ");
+            for(int j=0; j<3; j++) {
+                System.out.println(board[i][j] + " | ");
+            }
+            System.out.println("-------------------------");
+        }
+    }
+
+    // just for testing
+    public char[][] getBoard() {
+        return board;
+    }
 
 }
 
--- src/main/java/lab3/ui/Console.java
+++ src/main/java/lab3/ui/Console.java
@@ -1,8 +1,45 @@
 package lab3.ui;
-
+import java.util.Scanner;
 public class Console {
 
+    // Static Scanner object "sc" to take inputs
+    public static Scanner sc = new Scanner(System.in);
     // TODO: create a CLASS method to display a tictactoe board
-
+    public static void printBoard(char[][] board) {
+        System.out.println("---------------------");
+        for(int i=0; i<3; i++) {
+            System.out.print("| ");
+            for(int j=0; j<3; j++) {
+                System.out.println(board[i][j] + " | ");
+            }
+            System.out.println("-------------------------");
+        }
+    }
     // TODO: create a CLASS method to get the next move from a player
+    public static int[] getNextMove(char player) {
+        System.out.println("Player " + player + ", please enter your move (0,1,2): ");
+        int row=-1, col=-1; // initializing row and col with defaul values.
+
+        // looping using while
+        while (true) {
+            // try block: for exception handling
+            try {
+                row = sc.nextInt(); // taking user input for row
+                col = sc.nextInt(); // taking user input for col
+
+                // checking for upper and lower limits, MAX:3
+                if (row >= 0 && row < 3 && col >= 0 && col < 3) {
+                    break;
+                }
+                // in-case of error
+                else {
+                    System.out.println("Invalid input. Available options are: 0, 1 and 2.");
+                }
+            }
+            catch(Exception e) {
+                System.out.println(e.getMessage()); // printing stack-trace.
+            }
+        }
+        return new int[] {row, col}; // return row and col values to occupy space on the board.
+    }
 }
